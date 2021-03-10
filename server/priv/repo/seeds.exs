ec22268f8b0bc36543c7f53aadafdd2cbee6f2b5
Nat Tuck <nat@ferrus.net>
2021-03-10 16:18:24 -0500

    Handle the CORS issue.

--- server/priv/repo/seeds.exs
+++ server/priv/repo/seeds.exs
@@ -9,3 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias PhotoBlog.Repo
+alias PhotoBlog.Users.User
+
+alice = Repo.insert!(%User{name: "alice", password_hash: ""})
+bob = Repo.insert!(%User{name: "bob", password_hash: ""})
