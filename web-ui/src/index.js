b2ec57b483b4b117e531a5f52ebaa5dae6b05ab6
Nat Tuck <nat@ferrus.net>
2021-03-11 23:26:55 -0500

    Add Redux an an API module.

--- web-ui/src/index.js
+++ web-ui/src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store';
+import { load_defaults } from './api';
+
+load_defaults();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
